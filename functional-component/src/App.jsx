bdd8d8caa5b742cb19cb6a148092dcb4eb43e42c
madhav <madhav201singh@gmail.com>
2023-09-13 22:25:41 +0530

    first commit

---
+++ functional-component/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// Exercise 1: Create a Functional Component
+// Create a functional component called Greeting that renders a <div> containing a greeting message. 
+// The greeting message should be passed as a prop to the component. 
+
+function App() {
+  
+  return (
+    <>
+      
+    
+    </>
+  )
+}
+
+export default App
